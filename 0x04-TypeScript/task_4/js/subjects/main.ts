78fd93ce4b853de3f95e102856f707a9a363888d
DISCIPLINE55 <ismailmensah055@gmail.com>
2025-09-14 08:33:45 +0000

    Task_4 completed

---
+++ 0x04-TypeScript/task_4/js/subjects/main.ts
@@ -0,0 +1,33 @@
+// <reference path="./Cpp.ts" />
+// <reference path="./Java.ts" />
+// <reference path="./React.ts" />
+// <reference path="./Subject.ts" />
+// <reference path="./Teacher.ts" />
+
+export const cpp = new Subjects.Cpp();
+export const java = new Subjects.Java();
+export const react = new Subjects.React();
+
+export const cTeacher: Subjects.Teacher = {
+  firstName: 'John',
+  lastName: 'Doe',
+  experienceTeachingC: 10
+};
+
+// For Cpp subject
+console.log('C++');
+cpp.setTeacher(cTeacher);
+console.log(cpp.getRequirements());
+console.log(cpp.getAvailableTeacher());
+
+// For Java subject
+console.log('Java');
+java.setTeacher(cTeacher);
+console.log(java.getRequirements());
+console.log(java.getAvailableTeacher());
+
+// For React subject
+console.log('React');
+react.setTeacher(cTeacher);
+console.log(react.getRequirements());
+console.log(react.getAvailableTeacher());
